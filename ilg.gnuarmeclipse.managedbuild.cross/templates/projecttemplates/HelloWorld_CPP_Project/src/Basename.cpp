8888ae36c9cf2a836be5cf3c139adb206ba3b260
Liviu Ionescu <ilg@livius.net>
2013-11-10 23:24:28 +0200

    Hello World templates include the librdimon linker options to compile directly

--- ilg.gnuarmeclipse.managedbuild.cross/templates/projecttemplates/HelloWorld_CPP_Project/src/Basename.cpp
+++ ilg.gnuarmeclipse.managedbuild.cross/templates/projecttemplates/HelloWorld_CPP_Project/src/Basename.cpp
@@ -15,9 +15,11 @@ using namespace std;
 // On embedded platforms this might require semi-hosting or similar.
 //
 // For example, for toolchains derived from GNU Tools for Embedded,
-// to enable semi-hosting, the following should be added to the linker:
+// to enable semi-hosting, the following was added to the linker:
 //
 // --specs=rdimon.specs -Wl,--start-group -lgcc -lc -lc -lm -lrdimon -Wl,--end-group
+//
+// Adjust it for other toolchains.
 //
 
 int
