f4809419203ebd63ba75be4b41cba591626f8f5d
Liviu Ionescu <ilg@livius.net>
2014-02-04 22:12:52 +0200

    templates.core: conditional processes added

--- ilg.gnuarmeclipse.templates.core/src/ilg/gnuarmeclipse/templates/core/Utils.java
+++ ilg.gnuarmeclipse.templates.core/src/ilg/gnuarmeclipse/templates/core/Utils.java
@@ -4,17 +4,49 @@ import org.eclipse.cdt.managedbuilder.core.IConfiguration;
 
 public class Utils {
 
-	public static boolean isBuildType(IConfiguration config, String buildTypeSuffix) {
+	public static boolean isBuildType(IConfiguration config,
+			String buildTypeSuffix) {
 
-		if (buildTypeSuffix != null) {
-			buildTypeSuffix = buildTypeSuffix.trim();
-			String configBuildTypeValue = config.getBuildProperties()
-					.getProperty("org.eclipse.cdt.build.core.buildType")
-					.getValue().toString();
-			if (buildTypeSuffix.length() > 0 && configBuildTypeValue != null
-					&& configBuildTypeValue.endsWith(buildTypeSuffix)) {
-				return true;
-			}
+		if (buildTypeSuffix == null)
+			return true;
+
+		// If suffix not given, assume it is ok
+		buildTypeSuffix = buildTypeSuffix.trim();
+		if (buildTypeSuffix.length() == 0)
+			return true;
+
+		String configBuildTypeValue = config.getBuildProperties()
+				.getProperty("org.eclipse.cdt.build.core.buildType").getValue()
+				.toString();
+		if (configBuildTypeValue != null
+				&& configBuildTypeValue.endsWith(buildTypeSuffix)) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public static boolean isConditionSatisfied(String condition) {
+
+		if (condition == null)
+			return true;
+		
+		condition = condition.trim();
+		if (condition.length() == 0) {
+			// No condition string present, take it as ALWAYS
+			return true;
+		}
+
+		String sa[] = condition.split(" ");
+		if (sa.length != 3) {
+			System.out.println("Unrecognised condition "+condition);
+			return false;
+		}
+
+		if ("==".equals(sa[1])) {
+			return sa[0].equals(sa[2]);
+		} else if ("!=".equals(sa[1])) {
+			return !sa[0].equals(sa[2]);
 		}
 
 		return false;
