ca421b0f98f132cc812b2d2e81c768a37fb8ff47
Liviu Ionescu <ilg@livius.net>
2014-02-10 14:56:52 +0200

    templates: support for C++ projects
    - SetPropertyIfHasNature process added
    - freescale template implements C/C++

---
+++ ilg.gnuarmeclipse.templates.core/src/ilg/gnuarmeclipse/templates/core/processes/SetPropertyIfHasNature.java
@@ -0,0 +1,44 @@
+package ilg.gnuarmeclipse.templates.core.processes;
+
+import java.util.Map;
+
+import org.eclipse.cdt.core.templateengine.TemplateCore;
+import org.eclipse.cdt.core.templateengine.process.ProcessArgument;
+import org.eclipse.cdt.core.templateengine.process.ProcessFailureException;
+import org.eclipse.cdt.core.templateengine.process.ProcessRunner;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+
+public class SetPropertyIfHasNature extends ProcessRunner {
+
+	@Override
+	public void process(TemplateCore template, ProcessArgument[] args,
+			String processId, IProgressMonitor monitor)
+			throws ProcessFailureException {
+
+		String projectName = args[0].getSimpleValue();
+		String natureString = args[1].getSimpleValue();
+		String propertyName = args[2].getSimpleValue();
+		String propertyValue = args[3].getSimpleValue();
+
+		IProject projectHandle = ResourcesPlugin.getWorkspace().getRoot()
+				.getProject(projectName);
+		try {
+			if (projectHandle.hasNature(natureString)) {
+				//System.out.println("is " + natureString + " set "
+				//		+ propertyName + "=" + propertyValue);
+				
+				Map<String,String> values = template.getValueStore();
+				if (values.containsKey(propertyName)){
+					values.put(propertyName, propertyValue);
+				}
+			}
+		} catch (CoreException e1) {
+			System.out.println("has not nature");
+		}
+
+	}
+
+}
