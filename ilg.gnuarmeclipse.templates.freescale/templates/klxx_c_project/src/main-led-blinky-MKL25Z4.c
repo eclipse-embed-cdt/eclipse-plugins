84c6032087299c193b2a9fb1eb3afe81e124063a
Liviu Ionescu <ilg@livius.net>
2014-02-21 22:07:46 +0200

    freescale: fully configurable leds
    - mem.ld with more explanations for RAM
    - macros to configure led for port/pin numbers

--- ilg.gnuarmeclipse.templates.freescale/templates/klxx_c_project/src/main-led-blinky-MKL25Z4.c
+++ ilg.gnuarmeclipse.templates.freescale/templates/klxx_c_project/src/main-led-blinky-MKL25Z4.c
@@ -1,36 +1,40 @@
 
 /* ----- LED definitions --------------------------------------------------- */
 
-/* FRDM-KL25Z definitions (the GREEN LED, active low). */
+/* FRDM-KL25Z definitions (the GREEN LED, active low, B19). */
 /* (adjust them for your own board) */
 
-#define BLINK_PORT      		PTB
-#define BLINK_PIN       		19
-#define BLINK_SCGC5_MASK		SIM_SCGC5_PORTB_MASK
+/* Port numbers: 0=A, 1=B, 2=C, 3=D, 4=E */
+#define BLINK_PORT_NUMBER               (1)
+#define BLINK_PIN_NUMBER                (19)
+
+#define BLINK_PORTx(_N)                 ((PORT_Type *)(PORTA_BASE + (PORTB_BASE-PORTA_BASE)*(_N)))
+#define BLINK_GPIOx(_N)                 ((GPIO_Type *)(PTA_BASE + (PTB_BASE-PTA_BASE)*(_N)))
+#define BLINK_SCGC5_MASKx(_N)           (1 << ((SIM_SCGC5_PORTA_SHIFT) + (_N)))
 
 void
 blink_led_on()
 {
-  BLINK_PORT->PCOR = (1 << BLINK_PIN);
+  BLINK_GPIOx(BLINK_PORT_NUMBER)->PCOR = (1 << BLINK_PIN_NUMBER);
 }
 
 void
 blink_led_off()
 {
-  BLINK_PORT->PSOR = (1 << BLINK_PIN);
+  BLINK_GPIOx(BLINK_PORT_NUMBER)->PSOR = (1 << BLINK_PIN_NUMBER);
 }
 
 void
 blink_led_init()
 {
-  /* Turn on clock for BLINK_PORT module */
-  SIM->SCGC5 |= BLINK_SCGC5_MASK;
+  /* Turn on clock for port module */
+  SIM->SCGC5 |= BLINK_SCGC5_MASKx(BLINK_PORT_NUMBER);
  
-  /* Set the BLINK_PIN pin multiplexer to GPIO mode */
-  PORTB->PCR[BLINK_PIN] = PORT_PCR_MUX(1);
+  /* Set the pin multiplexer to GPIO mode */
+  BLINK_PORTx(BLINK_PORT_NUMBER)->PCR[BLINK_PIN_NUMBER] = PORT_PCR_MUX(1);
  
-  /* Set the BLINK_PIN as output */
-  BLINK_PORT->PDDR |= (1 << BLINK_PIN);
+  /* Set the pin as output */
+  BLINK_GPIOx(BLINK_PORT_NUMBER)->PDDR |= (1 << BLINK_PIN_NUMBER);
 
   /* Turn off led */
   blink_led_off();
