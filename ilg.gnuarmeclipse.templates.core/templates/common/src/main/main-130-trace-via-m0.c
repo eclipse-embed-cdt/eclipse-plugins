c6d1cb307339eee11a17b3e54fe2c6c0b3f1cfc1
Liviu Ionescu <ilg@livius.net>
2014-04-07 21:27:25 +0300

    templates.core: updated for Freescale
    - BlinkLed-*.h parts
    - sections .cfmconfig
    - main-130-trace-via-m0.c

---
+++ ilg.gnuarmeclipse.templates.core/templates/common/src/main/main-130-trace-via-m0.c
@@ -0,0 +1,6 @@
+// Trace support is enabled by adding the TRACE macro definition.
+// By default the trace messages are forwarded to the $(trace) output,
+// but can be rerouted to any device or completely suppressed, by
+// changing the definitions required in system/src/diag/trace_impl.c
+// (currently OS_USE_TRACE_SEMIHOSTING_DEBUG/_STDOUT).
+//
