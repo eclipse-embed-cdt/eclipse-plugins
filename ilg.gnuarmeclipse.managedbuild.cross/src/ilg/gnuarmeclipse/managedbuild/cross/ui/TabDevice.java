31208716f877d34fc121488561d4ec226d12188d
Liviu Ionescu <ilg@livius.net>
2014-07-18 22:06:48 +0300

    cross: TabDevices filled in, selections change content

--- ilg.gnuarmeclipse.managedbuild.cross/src/ilg/gnuarmeclipse/managedbuild/cross/ui/TabDevice.java
+++ ilg.gnuarmeclipse.managedbuild.cross/src/ilg/gnuarmeclipse/managedbuild/cross/ui/TabDevice.java
@@ -15,6 +15,8 @@ package ilg.gnuarmeclipse.managedbuild.cross.ui;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import ilg.gnuarmeclipse.managedbuild.cross.Activator;
 import ilg.gnuarmeclipse.packs.storage.PacksStorage;
@@ -28,9 +30,18 @@ import org.eclipse.cdt.managedbuilder.core.IConfiguration;
 import org.eclipse.cdt.managedbuilder.core.IToolChain;
 import org.eclipse.cdt.managedbuilder.internal.core.MultiConfiguration;
 import org.eclipse.cdt.managedbuilder.ui.properties.AbstractCBuildPropertyTab;
-import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.layout.TreeColumnLayout;
+import org.eclipse.jface.viewers.ColumnPixelData;
+import org.eclipse.jface.viewers.ILabelProviderListener;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.ITableLabelProvider;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
@@ -39,7 +50,9 @@ import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeColumn;
 
 /**
  * @noextend This class is not intended to be sub-classed by clients.
@@ -48,13 +61,15 @@ import org.eclipse.swt.widgets.Tree;
 @SuppressWarnings("restriction")
 public class TabDevice extends AbstractCBuildPropertyTab {
 
+	private static final int AUTOEXPAND_LEVEL = 2;
+
 	// ------------------------------------------------------------------------
 
 	class DevicesContentProvider extends NodeViewContentProvider {
 
 		@Override
 		public Object[] getChildren(Object parentElement) {
-			
+
 			Leaf[] children = (Leaf[]) super.getChildren(parentElement);
 
 			List<Leaf> list = new LinkedList<Leaf>();
@@ -72,19 +87,59 @@ public class TabDevice extends AbstractCBuildPropertyTab {
 		}
 	}
 
-	class DevicesLabelProvider extends LabelProvider {
+	class DevicesLabelProvider implements ITableLabelProvider {
 
-		public String getText(Object obj) {
+		public String getColumnText(Object obj, int columnIndex) {
+
 			Leaf node = (Leaf) obj;
-			String name = " " + node.getName();
-			if (node.isType(Type.FAMILY)) {
-				name += " (family)";
-			} else if (node.isType(Type.SUBFAMILY)) {
-				name += " (subfamily)";
-			} else if (node.isType(Type.VARIANT)) {
-				name += " (variant)";
+			String name = "";
+
+			switch (columnIndex) {
+
+			case 0:
+				name = " " + node.getName();
+				break;
+			case 1:
+				String description = node.getDescription();
+				if (description.length() == 0) {
+					if (node.isType(Type.VENDOR)) {
+						description = "Vendor";
+					} else if (node.isType(Type.FAMILY)) {
+						description = "Family";
+					} else if (node.isType(Type.SUBFAMILY)) {
+						description = "Subfamily";
+					} else if (node.isType(Type.DEVICE)) {
+						description = "Device";
+					} else if (node.isType(Type.VARIANT)) {
+						description = "Variant";
+					}
+				}
+				name = " " + description;
+				break;
 			}
 			return name;
+		}
+
+		@Override
+		public void addListener(ILabelProviderListener listener) {
+		}
+
+		@Override
+		public void dispose() {
+		}
+
+		@Override
+		public boolean isLabelProperty(Object element, String property) {
+			return false;
+		}
+
+		@Override
+		public void removeListener(ILabelProviderListener listener) {
+		}
+
+		@Override
+		public Image getColumnImage(Object element, int columnIndex) {
+			return null;
 		}
 	}
 
@@ -93,6 +148,7 @@ public class TabDevice extends AbstractCBuildPropertyTab {
 	TreeViewer fDevicesTree;
 	Label fArchitectureLabel;
 	Label fDeviceLabel;
+	Table fMemoryTable;
 	Button fMemoryEditButton;
 
 	// ------------------------------------------------------------------------
@@ -132,14 +188,14 @@ public class TabDevice extends AbstractCBuildPropertyTab {
 
 		// usercomp is defined in parent class
 
-		GridLayout layout = new GridLayout();
-		layout.numColumns = 1;
+		GridLayout gridLayout = new GridLayout();
+		gridLayout.numColumns = 1;
 		// layout.marginHeight = 0;
 		// layout.marginWidth = 0;
-		usercomp.setLayout(layout);
+		usercomp.setLayout(gridLayout);
 
-		GridData layoutData = new GridData(GridData.FILL_HORIZONTAL);
-		usercomp.setLayoutData(layoutData);
+		GridData gridData = new GridData(GridData.FILL_HORIZONTAL);
+		usercomp.setLayoutData(gridData);
 
 		createDeviceGroup(usercomp);
 
@@ -159,22 +215,39 @@ public class TabDevice extends AbstractCBuildPropertyTab {
 		group.setLayoutData(gridData);
 		group.setText(Messages.getString("DeviceTab_DeviceGroup_name"));
 
-		fDevicesTree = new TreeViewer(group, SWT.SINGLE | SWT.BORDER);
+		Composite treeComposite = new Composite(group, SWT.NONE);
+		fDevicesTree = new TreeViewer(treeComposite, SWT.SINGLE | SWT.BORDER);
+
 		gridData = new GridData();
 		gridData.grabExcessHorizontalSpace = true;
 		gridData.horizontalAlignment = SWT.FILL;
 		gridData.heightHint = 200;
 		gridData.horizontalSpan = 2;
+		treeComposite.setLayoutData(gridData);
+
 		Tree tree = fDevicesTree.getTree();
-		tree.setLayoutData(gridData);
 		tree.setHeaderVisible(true);
 		tree.setLinesVisible(true);
+
+		TreeColumnLayout layout = new TreeColumnLayout();
+		treeComposite.setLayout(layout);
+
+		TreeColumn nameColumn = new TreeColumn(tree, SWT.NONE);
+		nameColumn.setText("  Name");
+		layout.setColumnData(nameColumn, new ColumnPixelData(200));
+
+		TreeColumn descriptionColumn = new TreeColumn(tree, SWT.NONE);
+		descriptionColumn.setAlignment(SWT.LEFT);
+		descriptionColumn.setText(" Details");
+		// descriptionColumn.setWidth(450);
+		layout.setColumnData(descriptionColumn, new ColumnPixelData(450));
 
 		DevicesContentProvider contentProvider = new DevicesContentProvider();
 		fDevicesTree.setContentProvider(contentProvider);
 
 		fDevicesTree.setLabelProvider(new DevicesLabelProvider());
-		// fDevicesTree.setInput(getDevicesTree());
+
+		fDevicesTree.setAutoExpandLevel(AUTOEXPAND_LEVEL);
 
 		// ---
 
@@ -184,6 +257,24 @@ public class TabDevice extends AbstractCBuildPropertyTab {
 
 		fArchitectureLabel = new Label(group, SWT.NONE);
 		fArchitectureLabel.setText("-");
+
+		gridData = new GridData();
+		gridData.grabExcessHorizontalSpace = true;
+		gridData.horizontalAlignment = SWT.FILL;
+		fArchitectureLabel.setLayoutData(gridData);
+
+		// --------------------------------------------------------------------
+
+		fDevicesTree
+				.addSelectionChangedListener(new ISelectionChangedListener() {
+
+					@Override
+					public void selectionChanged(SelectionChangedEvent event) {
+
+						devicesTreeSelectionChanged(event);
+
+					}
+				});
 	}
 
 	private void createMemoryGroup(Composite parent) {
@@ -201,14 +292,18 @@ public class TabDevice extends AbstractCBuildPropertyTab {
 		{
 			fDeviceLabel = new Label(group, SWT.NONE);
 			fDeviceLabel.setText("-");
+
+			gridData = new GridData();
+			gridData.grabExcessHorizontalSpace = true;
+			gridData.horizontalAlignment = SWT.FILL;
+			fDeviceLabel.setLayoutData(gridData);
 		}
 
 		{
-			Table m_table;
-			m_table = new Table(group, SWT.SINGLE | SWT.BORDER
+			fMemoryTable = new Table(group, SWT.SINGLE | SWT.BORDER
 					| SWT.FULL_SELECTION);
-			m_table.setHeaderVisible(true);
-			m_table.setLinesVisible(true);
+			fMemoryTable.setHeaderVisible(true);
+			fMemoryTable.setLinesVisible(true);
 
 			gridData = new GridData();
 			// gridData.verticalAlignment = SWT.FILL;
@@ -216,34 +311,37 @@ public class TabDevice extends AbstractCBuildPropertyTab {
 			gridData.horizontalAlignment = SWT.FILL;
 			gridData.grabExcessHorizontalSpace = true;
 			gridData.heightHint = 62;
-			m_table.setLayoutData(gridData);
+			fMemoryTable.setLayoutData(gridData);
 
-			TableColumn m_columnName = new TableColumn(m_table, SWT.NULL);
-			m_columnName.setText("Name");
-			m_columnName.setWidth(80);
-			m_columnName.setResizable(true);
+			TableColumn m_columnSection = new TableColumn(fMemoryTable,
+					SWT.NULL);
+			m_columnSection.setText("Section");
+			m_columnSection.setWidth(80);
+			m_columnSection.setResizable(true);
 
-			TableColumn m_columnStart = new TableColumn(m_table, SWT.NULL);
+			TableColumn m_columnStart = new TableColumn(fMemoryTable, SWT.NULL);
 			m_columnStart.setText("Start");
 			m_columnStart.setWidth(100);
 			m_columnStart.setResizable(true);
 
-			TableColumn m_columnSize = new TableColumn(m_table, SWT.NULL);
+			TableColumn m_columnSize = new TableColumn(fMemoryTable, SWT.NULL);
 			m_columnSize.setText("Size");
 			m_columnSize.setWidth(100);
 			m_columnSize.setResizable(true);
 
-			TableColumn m_columnInit = new TableColumn(m_table, SWT.NULL);
+			TableColumn m_columnInit = new TableColumn(fMemoryTable, SWT.NULL);
 			m_columnInit.setText("Init");
 			m_columnInit.setWidth(55);
 			m_columnInit.setResizable(true);
 
-			TableColumn m_columnStartup = new TableColumn(m_table, SWT.NULL);
+			TableColumn m_columnStartup = new TableColumn(fMemoryTable,
+					SWT.NULL);
 			m_columnStartup.setText("Startup");
 			m_columnStartup.setWidth(55);
 			m_columnStartup.setResizable(true);
 
-			TableColumn m_columnDefault = new TableColumn(m_table, SWT.NULL);
+			TableColumn m_columnDefault = new TableColumn(fMemoryTable,
+					SWT.NULL);
 			m_columnDefault.setText("Default");
 			m_columnDefault.setWidth(55);
 			m_columnDefault.setResizable(true);
@@ -254,6 +352,162 @@ public class TabDevice extends AbstractCBuildPropertyTab {
 			fMemoryEditButton.setText(Messages
 					.getString("DeviceTab_MemoryGroup_edit_button"));
 		}
+	}
+
+	// -------------------------------------------------------------------------
+
+	// Called for each selection in the devices tree
+	private void devicesTreeSelectionChanged(SelectionChangedEvent event) {
+
+		IStructuredSelection selection = (IStructuredSelection) event
+				.getSelection();
+
+		// System.out.println(selection);
+
+		Object element = selection.getFirstElement();
+		if (!(element instanceof Node)) {
+			return;
+		}
+
+		Node node = (Node) element;
+		if (node.isType(Type.DEVICE) && node.hasChildren()) {
+			for (Leaf child : node.getChildren()) {
+				if (child.isType(Type.VARIANT)) {
+					// Do not allow selections for devices that have variants
+					fDevicesTree.setSelection(StructuredSelection.EMPTY);
+					return;
+				}
+			}
+		}
+
+		if (!(node.isType(Type.DEVICE) || node.isType(Type.VARIANT))) {
+			// Do not allow selections for other than devices or variants
+			fDevicesTree.setSelection(StructuredSelection.EMPTY);
+			return;
+		}
+
+		Node memNode = null;
+		if (node.getParent().isType(Type.BOARD)) {
+			System.out
+					.println("Board device " + element + " not yet processed");
+			// TODO: search for true device node
+		} else {
+			// System.out.println("Device " + element);
+			memNode = node;
+		}
+
+		Map<String, String[]> map = collectMemoryMap(memNode);
+
+		updateMemoryTableContent(map);
+
+		String arch = collectNodePropery(memNode, Node.CORE_PROPERTY, "");
+		fArchitectureLabel.setText(arch);
+
+		fDeviceLabel.setText(memNode.getName());
+	}
+
+	private Map<String, String[]> collectMemoryMap(Node node) {
+
+		// Collect the memory map from node and parents
+		Map<String, String[]> map = new TreeMap<String, String[]>();
+		if (node != null) {
+			do {
+				if (node.hasChildren()) {
+					for (Leaf child : node.getChildren()) {
+						if (child.isType(Type.MEMORY)) {
+							String key = child.getName();
+
+							String[] line;
+							if (map.containsKey(key)) {
+								line = map.get(key);
+							} else {
+								line = new String[] { "", "", "", "", "", "" };
+								line[0] = key;
+
+								map.put(key, line);
+							}
+
+							// 'line' is either new or partly filled in
+							// The order is
+							// section,start,size,init,startup,default
+							if (line[1].length() == 0) {
+								line[1] = child.getProperty(
+										Node.START_PROPERTY, "").trim();
+							}
+							if (line[2].length() == 0) {
+								line[2] = child.getProperty(Node.SIZE_PROPERTY,
+										"").trim();
+							}
+							if (line[3].length() == 0) {
+								line[3] = child.getProperty(Node.INIT_PROPERTY,
+										"").trim();
+							}
+							if (line[4].length() == 0) {
+								line[4] = child.getProperty(
+										Node.STARTUP_PROPERTY, "").trim();
+							}
+							if (line[5].length() == 0) {
+								line[5] = child.getProperty(
+										Node.DEFAULT_PROPERTY, "").trim();
+							}
+
+						}
+					}
+				}
+				// Iterate up
+				node = node.getParent();
+				// Stop when we reached the node above family node
+			} while (!node.isType(Type.NONE));
+		}
+
+		for (String[] line : map.values()) {
+
+			// Default [init,startup,default] values to 0
+			for (int i = 3; i <= 5; ++i) {
+				if (line[i].length() == 0) {
+					line[i] = "0";
+				}
+			}
+		}
+		return map;
+	}
+
+	private void updateMemoryTableContent(Map<String, String[]> map) {
+
+		fMemoryTable.removeAll();
+
+		if (map != null) {
+			TableItem item;
+			for (String key : map.keySet()) {
+				String[] line = map.get(key);
+				item = new TableItem(fMemoryTable, SWT.NULL);
+				item.setText(line);
+			}
+		}
+	}
+
+	private String collectNodePropery(Node node, String propertyName,
+			String defaultValue) {
+
+		String value = "";
+		if (node != null) {
+			do {
+
+				value = node.getProperty(propertyName, "");
+
+				if (value.length() > 0)
+					break;
+
+				// Iterate up
+				node = node.getParent();
+				// Stop when we reached the node above family node
+			} while (!node.isType(Type.NONE));
+		}
+
+		if (value.length() == 0) {
+			return defaultValue;
+		}
+		return value;
 	}
 
 	// -------------------------------------------------------------------------
