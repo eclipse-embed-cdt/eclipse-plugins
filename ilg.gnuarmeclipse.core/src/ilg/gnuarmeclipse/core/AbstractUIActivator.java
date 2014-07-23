84f476ff9395ebdffe7a8f2b7327a0e2e638e80d
Liviu Ionescu <ilg@livius.net>
2014-07-23 18:27:15 +0300

    ilg.gnuarmeclipse.core plug-in added

---
+++ ilg.gnuarmeclipse.core/src/ilg/gnuarmeclipse/core/AbstractUIActivator.java
@@ -0,0 +1,76 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Liviu Ionescu.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Liviu Ionescu - initial implementation.
+ *******************************************************************************/
+
+package ilg.gnuarmeclipse.core;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public abstract class AbstractUIActivator extends AbstractUIPlugin {
+
+	// ------------------------------------------------------------------------
+
+	private static AbstractUIActivator sfInstance;
+
+	public static AbstractUIActivator getInstance() {
+		return sfInstance;
+	}
+
+	// ------------------------------------------------------------------------
+
+	public abstract String getBundleId();
+
+	// ------------------------------------------------------------------------
+
+	public AbstractUIActivator() {
+
+		super();
+		sfInstance = this;
+	}
+
+	// ------------------------------------------------------------------------
+
+	public void start(BundleContext context) throws Exception {
+
+		System.out.println(getBundleId() + ".start()");
+		super.start(context);
+
+	}
+
+	public void stop(BundleContext context) throws Exception {
+
+		super.stop(context);
+		System.out.println(getBundleId() + ".stop()");
+	}
+
+	// ------------------------------------------------------------------------
+
+	public static void log(IStatus status) {
+		getInstance().getLog().log(status);
+	}
+
+	public static void log(Throwable e) {
+		log(new Status(IStatus.ERROR, getInstance().getBundleId(), 1,
+				"Internal Error", e)); //$NON-NLS-1$
+	}
+
+	public static void log(String message) {
+		log(new Status(IStatus.ERROR, getInstance().getBundleId(), 1, message,
+				null)); //$NON-NLS-1$
+	}
+
+	// ------------------------------------------------------------------------
+}
