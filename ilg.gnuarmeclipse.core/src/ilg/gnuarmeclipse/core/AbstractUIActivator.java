502f289dd3d8ae53770948a07b2c845cacd74278
Liviu Ionescu <ilg@livius.net>
2015-01-19 15:40:27 +0200

    core: getImage() & co. added to UIActivator

--- ilg.gnuarmeclipse.core/src/ilg/gnuarmeclipse/core/AbstractUIActivator.java
+++ ilg.gnuarmeclipse.core/src/ilg/gnuarmeclipse/core/AbstractUIActivator.java
@@ -11,8 +11,14 @@
 
 package ilg.gnuarmeclipse.core;
 
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.ui.ISharedImages;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
@@ -54,6 +60,110 @@ public abstract class AbstractUIActivator extends AbstractUIPlugin {
 
 		super.stop(context);
 		System.out.println(getBundleId() + ".stop()");
+	}
+
+	// ------------------------------------------------------------------------
+
+	/**
+	 * For a given name, get the 'icons/name.png' description from the registry.
+	 * If not there, register it.
+	 * 
+	 * @param name
+	 *            a String with the file name (default .png).
+	 * @return an ImageDescriptor or null.
+	 */
+	public ImageDescriptor getImageDescriptor(String name) {
+
+		String str = name.toLowerCase();
+		ImageDescriptor imageDescriptor = getImageRegistry().getDescriptor(str);
+		if (imageDescriptor == null) {
+			imageDescriptor = declareImage(str);
+		}
+		return imageDescriptor;
+	}
+
+	/**
+	 * For a given name, get the 'icons/name.png' image from the registry. If
+	 * not there, register it.
+	 * 
+	 * @param name
+	 *            a String with the file name (default .png).
+	 * @return an Image, possibly a default one.
+	 */
+	public Image getImage(String name) {
+
+		String str = name.toLowerCase();
+
+		Image image = getImageRegistry().get(str);
+		if (image != null) {
+			// The common case, when the image exist; return it.
+			return image;
+		}
+
+		// Image not known, register it.
+		ImageDescriptor imageDescriptor = declareImage(str);
+		if (imageDescriptor == null) {
+			return PlatformUI.getWorkbench().getSharedImages()
+					.getImage(ISharedImages.IMG_OBJS_ERROR_TSK);
+		}
+		image = getImageRegistry().get(str);
+		return image;
+	}
+
+	/**
+	 * For a given 'name', register 'icons/name.png' in the local image
+	 * registry.
+	 * 
+	 * @param name
+	 *            a String with the file name (default .png).
+	 * @return an ImageDescriptor or null.
+	 */
+	protected ImageDescriptor declareImage(String name) {
+
+		Object path = new Path("icons/", name);
+		Object pathx = path;
+		String extension = ((IPath) path).getFileExtension();
+		if ((extension == null) || (extension.isEmpty())) {
+			// If missing, try default extension '.png'.
+			pathx = ((IPath) path).addFileExtension("png");
+		}
+
+		// Check path in the plug-in name space.
+		ImageDescriptor imageDescriptor = AbstractUIPlugin
+				.imageDescriptorFromPlugin(getBundleId(),
+						((IPath) pathx).toString());
+		if (imageDescriptor == null) {
+			if ((extension == null) || (extension.isEmpty())) {
+				// If missing, second default extension is '.gif'.
+				pathx = ((IPath) path).addFileExtension("gif");
+
+				// Check gif path in the plug-in name space.
+				imageDescriptor = AbstractUIPlugin.imageDescriptorFromPlugin(
+						getBundleId(), ((IPath) pathx).toString());
+			}
+			if (imageDescriptor == null) {
+				return null;
+			}
+		}
+		try {
+			String key = getKey(name);
+			if (getImageRegistry().getDescriptor(key) == null) {
+				getImageRegistry().put(key, imageDescriptor);
+			}
+		} catch (Exception e) {
+			log(e);
+		}
+		return imageDescriptor;
+	}
+
+	/**
+	 * The key is based on the image name.
+	 * 
+	 * @param name
+	 * @return
+	 */
+	private String getKey(String name) {
+		return name.toLowerCase();
 	}
 
 	// ------------------------------------------------------------------------
