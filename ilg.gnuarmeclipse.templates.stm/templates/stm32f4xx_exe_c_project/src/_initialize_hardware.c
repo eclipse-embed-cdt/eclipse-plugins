98d1803dc13e14c72ef8526f91144bfa19fdb5b1
Liviu Ionescu <ilg@livius.net>
2015-08-03 23:51:57 +0300

    template.stm: SystemClock_Config() for HSI
    - HAL_Init() moved first
    - more elaborate SystemClock_Config()

--- ilg.gnuarmeclipse.templates.stm/templates/stm32f4xx_exe_c_project/src/_initialize_hardware.c
+++ ilg.gnuarmeclipse.templates.stm/templates/stm32f4xx_exe_c_project/src/_initialize_hardware.c
@@ -36,7 +36,7 @@ void
 __initialize_hardware(void);
 
 void
-configure_system_clock(void);
+SystemClock_Config(void);
 
 // ----------------------------------------------------------------------------
 
@@ -48,37 +48,35 @@ configure_system_clock(void);
 //
 // After Reset the Cortex-M processor is in Thread mode,
 // priority is Privileged, and the Stack is set to Main.
+//
+// Warning: The HAL requires the system timer, running at 1000 Hz
+// and calling HAL_IncTick().
 
 void
 __initialize_hardware(void)
 {
+  // Initialise the HAL Library; it must be the first function
+  // to be executed before the call of any HAL function.
+  HAL_Init();
+
   // Enable HSE Oscillator and activate PLL with HSE as source
-  configure_system_clock();
+  SystemClock_Config();
 
   // Call the CSMSIS system clock routine to store the clock frequency
   // in the SystemCoreClock global RAM location.
   SystemCoreClockUpdate();
-
-  // Initialise the HAL Library; it must be the first
-  // instruction to be executed in the main program.
-  HAL_Init();
-
-  // Warning: The HAL always initialises the system timer.
-  // For this to work, the default SysTick_Handler must not hang
-  // (see system/src/cortexm/exception_handlers.c)
-
-  // Unless explicitly enabled by the application, we prefer
-  // to keep the timer interrupts off.
-  HAL_SuspendTick();
 }
 
+// Disable when using RTOSes, since they have their own handler.
 #if 0
 
 // This is a sample SysTick handler, use it if you need HAL timings.
 void __attribute__ ((section(".after_vectors")))
 SysTick_Handler(void)
 {
+#if defined(USE_HAL_DRIVER)
 	HAL_IncTick();
+#endif
 }
 
 #endif
@@ -106,11 +104,8 @@ SysTick_Handler(void)
  * @retval None
  */
 void
-configure_system_clock(void)
+SystemClock_Config(void)
 {
-  RCC_ClkInitTypeDef RCC_ClkInitStruct;
-  RCC_OscInitTypeDef RCC_OscInitStruct;
-
   // Enable Power Control clock
   __PWR_CLK_ENABLE();
 
@@ -120,12 +115,15 @@ configure_system_clock(void)
   // datasheet.
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-  // Enable HSE Oscillator and activate PLL with HSE as source
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+
+#if defined(HSE_VALUE) && (HSE_VALUE != 0)
+  // Enable HSE Oscillator and activate PLL with HSE as source.
+  // This is tuned for STM32F4-DISCOVERY; update it for your board.
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-
   // This assumes the HSE_VALUE is a multiple of 1MHz. If this is not
   // your case, you have to recompute these PLL constants.
   RCC_OscInitStruct.PLL.PLLM = (HSE_VALUE/1000000u);
@@ -133,16 +131,46 @@ configure_system_clock(void)
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
   RCC_OscInitStruct.PLL.PLLQ = 7;
   HAL_RCC_OscConfig(&RCC_OscInitStruct);
+#else
+  // Use HSI and activate PLL with HSI as source.
+  // This is tuned for NUCLEO-F411; update it for your board.
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = 16;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  // This assumes the HSI_VALUE is a multiple of 1MHz. If this is not
+  // your case, you have to recompute these PLL constants.
+  RCC_OscInitStruct.PLL.PLLM = (HSI_VALUE/1000000u);
+  RCC_OscInitStruct.PLL.PLLN = 336;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  HAL_RCC_OscConfig(&RCC_OscInitStruct);
+#endif
 
+
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
   // Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2
   // clocks dividers
   RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK
       | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+#if define(STM32F411xE)
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2);
+#else
+  // This is tuned for STM32F407; update it for your device.
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
   HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5);
+#endif
+
+  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+
+  HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
 }
 
 // ----------------------------------------------------------------------------
