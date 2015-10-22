5136d3b5d1f116958ce2cc4d9ee897896da40a7c
Liviu Ionescu <ilg@livius.net>
2015-10-22 23:10:45 +0300

    templates.stm: f7 .cpp led I1 definition fixed
    - PI1 is port# 8, not 9

--- ilg.gnuarmeclipse.templates.stm/templates/stm32f7xx_exe_c_project/src/main/main-145-leds-blinky.cpp
+++ ilg.gnuarmeclipse.templates.stm/templates/stm32f7xx_exe_c_project/src/main/main-145-leds-blinky.cpp
@@ -5,7 +5,7 @@
 
 #warning "Assume a STM32F746G-DISCO board, PI1, active high."
 
-#define BLINK_PORT_NUMBER         (9)
+#define BLINK_PORT_NUMBER         (8)
 #define BLINK_PIN_NUMBER_GREEN    (1)
 #define BLINK_ACTIVE_LOW          (false)
 
