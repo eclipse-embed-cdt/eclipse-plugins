70b88833a18094cbc1d263cd2c42a9b2b7704efd
Liviu Ionescu <ilg@livius.net>
2015-10-23 10:45:25 +0300

    templates.stm: f7 add STM32F746_EVAL leds

--- ilg.gnuarmeclipse.templates.stm/templates/stm32f7xx_exe_c_project/src/BlinkLed-40-defs.h
+++ ilg.gnuarmeclipse.templates.stm/templates/stm32f7xx_exe_c_project/src/BlinkLed-40-defs.h
@@ -1,13 +1,31 @@
 
+// Ports numbers are: PA=0, PB=1, PC=2, PD=3, PE=4, PF=5, PG=6, PH=7, PI=8.
+
 #if defined(STM32F746xx)
+
+#if defined(STM32F746_EVAL)
+
+//#warning "Assume a STM32F746G-EVAL board, PF12 & PB7, active low."
+
+#define BLINK_PORT_NUMBER         (5)
+#define BLINK_PIN_NUMBER          (12)
+
+#define BLINK_PORT_NUMBER_RED     (1)
+#define BLINK_PIN_NUMBER_RED      (7)
+
+#define BLINK_ACTIVE_LOW          (1)
+
+#else
 
 #warning "Assume a STM32F746G-DISCO board, PI1, active high."
 
 // STM32F746-DISCO definitions (the GREEN led, I1, active high)
 
-#define BLINK_PORT_NUMBER               (9)
+#define BLINK_PORT_NUMBER               (8)
 #define BLINK_PIN_NUMBER                (1)
 #define BLINK_ACTIVE_LOW                (0)
+
+#endif
 
 #else
 
