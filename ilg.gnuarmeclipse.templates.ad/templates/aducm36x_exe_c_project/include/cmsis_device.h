ef30e2f75841192d5a5ea1bb23a1ccfc4c924720
Liviu Ionescu <ilg@livius.net>
2015-10-23 12:35:21 +0300

    templates.ad: fix warnings
    - in cmsis_device.h
    - with extra -Wno-* for library * system_*

--- ilg.gnuarmeclipse.templates.ad/templates/aducm36x_exe_c_project/include/cmsis_device.h
+++ ilg.gnuarmeclipse.templates.ad/templates/aducm36x_exe_c_project/include/cmsis_device.h
@@ -1,6 +1,11 @@
 #ifndef _CMSIS_H_
 #define _CMSIS_H_
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wpadded"
+
 #include "$(CMSIS_name).h"
+
+#pragma GCC diagnostic pop
 
 #endif // _CMSIS_H_
