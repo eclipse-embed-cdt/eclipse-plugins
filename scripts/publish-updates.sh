efca1e1a639992bd0a32e97dd0e5f0fabd319a6d
Liviu Ionescu <ilg@livius.net>
2017-04-04 10:30:00 +0300

    publish-updates.sh: publish to bintray

--- scripts/publish-updates.sh
+++ scripts/publish-updates.sh
@@ -30,9 +30,162 @@ then
   echo "Force write"
 fi
 
+CURL_OPTS=-#
+
+# -----------------------------------------------------------------------------
+
+# POST /packages/:subject/:repo/:package/versions
+# {
+#   "name": "1.1.5",
+#   "released": "ISO8601 (yyyy-MM-dd'T'HH:mm:ss.SSSZ)", (optional)
+#   "desc": "This version...",
+#   "github_release_notes_file": "RELEASE.txt", (optional)
+#   "github_use_tag_release_notes": true, (optional)
+#   "vcs_tag": "1.1.5" (optional)
+# }
+
+# $1 = repo 
+# $2 = package
+# $3 = version
+do_create_version()
+{
+  local repo="$1"
+  local package="$2"
+  local version="$3"
+
+  echo "Create version '${repo}/${package}/${version}'"
+
+  local tmp_path="$(mktemp)"
+  # Note: EOF is not quoted to allow local substitutions.
+  cat <<__EOF__ >"${tmp_path}"
+{ 
+  "name": "${version}"
+}
+__EOF__
+
+  local ret="$(curl ${CURL_OPTS} \
+    --request POST \
+    --user ${BINTRAY_USER}:${BINTRAY_API_KEY} \
+    --header "Content-Type: application/json" \
+    --data @"${tmp_path}" \
+    "${API}/packages/${BINTRAY_OWNER}/${repo}/${package}/versions")"
+
+  if [ "${ret:0:8}" == '{"name":' ]
+  then
+    echo "Created."
+  else
+    echo "${ret}"
+    failure="-y"
+  fi
+
+  rm "${tmp_path}"
+
+  echo
+}
+
+# X-Bintray-Package: :package
+# X-Bintray-Version: :version
+# [X-Bintray-Publish: 0/1]
+# [X-Bintray-Override: 0/1]
+# [X-Bintray-Explode: 0/1]
+# PUT /content/:subject/:repo/:file_path
+
+# $1 = file absolute path
+# $2 = repo 
+# $3 = package
+# $4 = version
+# $5 = file relative path
+do_upload_to_pv_header()
+{
+  local file_absolute_path="$1"
+  local repo="$2"
+  local package="$3"
+  local version="$4"
+  local file_relative_path="$5"
+
+  echo "Upload '${file_relative_path}' to '/${repo}/${package}/${version}/'"
+  local ret="$(curl ${CURL_OPTS} \
+    --request PUT \
+    --upload-file "${file_absolute_path}" \
+    --user ${BINTRAY_USER}:${BINTRAY_API_KEY} \
+    --header "X-Bintray-Package: ${package}" \
+    --header "X-Bintray-Version: ${version}" \
+    --header "X-Bintray-Publish: 0" \
+    --header "X-Bintray-Override: 1" \
+    --header "X-Bintray-Explode: 0" \
+    "${API}/content/${BINTRAY_OWNER}/${repo}/${file_relative_path}")"
+
+  if [ "${ret}" == '{"message":"success"}' ]
+  then
+    echo "Uploaded."
+  else
+    failure="-y"
+    echo "${ret}"
+  fi
+}
+
+# POST /content/:subject/:repo/:package/:version/publish
+# $1 = repo 
+# $2 = package
+# $3 = version
+do_publish()
+{
+  local repo="$1"
+  local package="$2"
+  local version="$3"
+
+  echo "Publish '${repo}/${package}/${version}'"
+  curl ${CURL_OPTS} \
+    --request POST \
+    --user ${BINTRAY_USER}:${BINTRAY_API_KEY} \
+    --header "Content-Type: application/json" \
+    -d "{ \"publish_wait_for_secs\": -1, \"discard\": \"false\" }" \
+    "${API}/content/${BINTRAY_OWNER}/${repo}/${package}/${version}/publish"
+
+  echo 
+}
+
+# $1 = repo name
+# $2 = full version 
+do_upload_to_bintray()
+{
+  local repo="$1"
+  local package="p2"
+  local version="$2"
+  failure=""
+  do_create_version "${repo}" "${package}" "${version}"
+
+  for f in *.* */*
+  do
+    if [ -z "${failure}" ]
+    then
+      do_upload_to_pv_header "$(pwd)/${f}" "${repo}" "${package}" "${version}" "${f}"
+    fi
+  done
+
+  if [ -z "${failure}" ]
+  then
+    do_publish "${repo}" "${package}" "${version}"
+  fi
+
+  echo
+}
+
+# -----------------------------------------------------------------------------
+
 SF_USER=ilg-ul
 SF_DESTINATION="$SF_USER,gnuarmeclipse@frs.sourceforge.net:/home/frs/project/g/gn/gnuarmeclipse/$SF_FOLDER$TEST"
 SOURCE_LIST="."
+
+BINTRAY_USER=ilg-ul
+BINTRAY_OWNER=gnuarmeclipse
+
+API=https://api.bintray.com
+
+echo
+echo "User: ${BINTRAY_USER}"
+echo "API key: ${BINTRAY_API_KEY}"
+echo "Owner: ${BINTRAY_OWNER}"
 
 # -c skip based on checksum, not mod-time & size
 
@@ -59,6 +212,9 @@ fi
 
 cd ../ilg.gnuarmeclipse-repository/target
 
+FULL_VERSION="$(grep "unit id='ilg.gnuarmeclipse.core'" targetPlatformRepository/content.xml | sed "s/.*version='\(.*\)'.*/\1/")"
+(cd repository; do_upload_to_bintray "updates${TEST}" "${FULL_VERSION}")
+
 echo "Rsync-ing SourceForge ${SF_FOLDER}${TEST} site (${RSYNC_OPTS})"
 (cd repository; rsync -e ssh ${RSYNC_OPTS} ${SOURCE_LIST} ${SF_DESTINATION})
 
@@ -74,7 +230,7 @@ then
   NUMDATE=$(ls repository/plugins/ilg.gnuarmeclipse.managedbuild.cross* | sed -e 's/.*_[0-9]*[.][0-9]*[.][0-9]*[.]\([0-9]*\)[.]jar/\1/')
   ARCHIVE_PREFIX=$(ls *-SNAPSHOT.zip | sed -e 's/\(.*\)-SNAPSHOT[.]zip/\1/')
 
-  ARCHIVE_FOLDER="../../../../archive"
+  ARCHIVE_FOLDER="../../../archive"
   if [ "${TEST}" == "-test" ]
   then
     ARCHIVE_FOLDER="${ARCHIVE_FOLDER}/internal"
