e0b648f2463c74cbfed2f4fcd0b164966b293f1f
Liviu Ionescu <ilg@livius.net>
2017-12-09 02:37:38 +0200

    packs.core: add PackTypes

---
+++ bundles/ilg.gnumcueclipse.packs.core/src/ilg/gnumcueclipse/packs/core/PackTypes.java
@@ -0,0 +1,17 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Liviu Ionescu.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Liviu Ionescu - initial implementation.
+ *******************************************************************************/
+
+package ilg.gnumcueclipse.packs.core;
+
+public class PackTypes {
+	public static final String CMSIS = "cmsis";
+	public static final String XPACK_XCDL = "xpack.xcdl";
+}
