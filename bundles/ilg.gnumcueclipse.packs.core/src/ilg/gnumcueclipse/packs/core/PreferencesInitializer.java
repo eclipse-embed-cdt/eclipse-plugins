4345cd17da89f9b2d3601b42a50f0f4b9c74dce9
Liviu Ionescu <ilg@livius.net>
2019-09-11 12:03:09 +0300

    [#357]: Change the CMSIS Packs default location

--- bundles/ilg.gnumcueclipse.packs.core/src/ilg/gnumcueclipse/packs/core/PreferencesInitializer.java
+++ bundles/ilg.gnumcueclipse.packs.core/src/ilg/gnumcueclipse/packs/core/PreferencesInitializer.java
@@ -12,12 +12,17 @@
 package ilg.gnumcueclipse.packs.core;
 
 import java.io.File;
+import java.util.Map;
 
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
 import org.eclipse.jface.preference.IPreferenceStore;
+
+import ilg.gnumcueclipse.core.EclipseUtils;
+import ilg.gnumcueclipse.core.XpackUtils;
 
 /**
  * Class used to initialise default preference values.
@@ -26,31 +31,60 @@ public class PreferencesInitializer extends AbstractPreferenceInitializer {
 
 	public void initializeDefaultPreferences() {
 
-		// System.out
-		// .println("PreferenceInitializer.initializeDefaultPreferences()");
-
+		if (Activator.getInstance().isDebugging()) {
+			System.out.println("PreferenceInitializer.initializeDefaultPreferences()");
+		}
+		
 		// Get workspace
 		IWorkspace workspace = ResourcesPlugin.getWorkspace();
 
-		// Compute workspace/../Packages
-		IPath workspacePath = workspace.getRoot().getLocation();
-		IPath workspaceParentPath = workspacePath.removeLastSegments(1);
-		IPath packagesPath = workspaceParentPath.append("Packages");
-		// System.out.println(packagesPath.toOSString());
+		IPath packagesPath = null;
 
+		Map<String, String> env = System.getenv();
+		String envFolder = env.get("CMSIS_PACKS_FOLDER");
+		if (envFolder != null) {
+			IPath path = new Path(envFolder);
+			if (path.toFile().isDirectory()) {
+				packagesPath = path;
+			} else {
+				Activator.log("\"" + envFolder + "\" not a folder, ignored.");
+			}
+		}
+
+		// For compatibility reasons, if workspace/../Packages exists, use it.
+		if (packagesPath == null) {
+			IPath workspacePath = workspace.getRoot().getLocation();
+			IPath workspaceParentPath = workspacePath.removeLastSegments(1);
+			IPath path = workspaceParentPath.append("Packages");
+			// System.out.println(packagesPath.toOSString());
+			File packagesFolder = new File(path.toOSString());
+			if (packagesFolder.isDirectory()) {
+				packagesPath = path;
+			}
+		}
+
+		if (packagesPath == null) {
+			packagesPath = XpackUtils.getRepoBasePath();
+			if (EclipseUtils.isLinux()) {
+				packagesPath = packagesPath.append("cmsis-packs");
+			} else {
+				packagesPath = packagesPath.append("CMSIS-Packs");
+			}
+		}
+		
 		IPreferenceStore store = Preferences.getPreferenceStore();
 		store.setDefault(Preferences.PACKS_FOLDER_PATH, packagesPath.toOSString());
 		store.setDefault(Preferences.PACKS_MACRO_NAME, Preferences.DEFAULT_MACRO_NAME);
-		
+
 		// Read back the actual value.
 		String folderPath = store.getString(Preferences.PACKS_FOLDER_PATH);
 
 		File packagesFolder = new File(folderPath);
 		if (!packagesFolder.exists()) {
 			packagesFolder.mkdir();
-			if (Activator.getInstance().isDebugging()) {
-			  System.out.println(packagesFolder + " created");
-			}
+			System.out.println("Folder \"" + packagesFolder + "\" created");
+		} if (!packagesFolder.isDirectory()) {
+			Activator.log("\"" + folderPath + "\" present, but not a folder; not good.");
 		}
 	}
 }
