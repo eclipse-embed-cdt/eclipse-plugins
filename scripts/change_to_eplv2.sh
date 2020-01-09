7c6928e78a45da227a0680ca432c5ae5a8580b0b
Jonah Graham <jonah@kichwacoders.com>
2020-01-09 10:09:29 -0500

    Changes to the EPL script to catch GNU MCU cases
    In addition to what was done for CDT:
    - Skip all C and H files as they are from ARM
    - Change Eclipse Distribution License v1.0 to epl also
    - fixup feature.properties as whole license is in there
      (CDT and Eclipse platform don
    - A few other small pattern changes that were not present in CDT

--- scripts/change_to_eplv2.sh
+++ scripts/change_to_eplv2.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-
-find . -type f ! -name "change_to_eplv2.sh" ! -path "./.git/*" ! \( -name debug.product -name "*.exsd" -or -name cdt_c_over_cdt.htm \) \
+find . -type f ! -name "change_to_eplv2.sh" ! -path "./.git/*" ! \( -name "*.exsd" -or -name "*.properties" -or -name "*.c" -or -name "*.h" \) \
   -exec sed -i -E \
     -e 's@Eclipse Public License v1.0@Eclipse Public License 2.0@g' \
+    -e 's@Eclipse Distribution License v1.0@Eclipse Public License 2.0@g' \
     -e 's@^([[:blank:]]*[^[:blank:]])([[:blank:]]*)(All rights reserved. )@\1\n\1\2@' \
     -e 's@\.[[:blank:]]*All rights reserved.@\.@' \
     -e 's@All rights reserved.([[:blank:]]*)@@' \
     -e 's@\&lt;a href=\&quot;http://www.eclipse.org/legal/epl-v10.html\&quot;\&gt;@\&lt;a href=\&quot;http://www.eclipse.org/legal/epl-2.0/\&quot;\&gt;@' \
     -e 's@at http://www.eclipse.org/legal/epl-v10.html@at https://www.eclipse.org/legal/epl-2.0/@' \
     -e 's@([[:blank:]]*[^[:blank:]]{1,2})([[:blank:]]*)http://www.eclipse.org/legal/epl-v10.html@\1\2https://www.eclipse.org/legal/epl-2.0/\n\1\n\1\2SPDX-License-Identifier: EPL-2.0@' \
+    -e 's@([[:blank:]]*)http://www.eclipse.org/org/documents/edl-v10.php@\1https://www.eclipse.org/legal/epl-2.0/\n\1\n\1SPDX-License-Identifier: EPL-2.0@' \
     {} +
 
-find . -type f ! -name "change_to_eplv2.sh" ! -path "./.git/*" \( -name debug.product -name "*.exsd" -or -name cdt_c_over_cdt.htm -or -name "*.properties" \) \
+find . -type f ! -name "change_to_eplv2.sh" ! -path "./.git/*" \( -name "*.exsd" -or -name "*.properties" \) \
   -exec sed -i -E \
+    -e 's@Eclipse Public License 1.0@Eclipse Public License 2.0@g' \
     -e 's@Eclipse Public License v1.0@Eclipse Public License 2.0@g' \
     -e 's@^([[:blank:]]*[^[:blank:]])([[:blank:]]*)(All rights reserved. )@\1\n\1\2@' \
     -e 's@\.[[:blank:]]*All rights reserved.@\.@' \
@@ -21,7 +23,14 @@ find . -type f ! -name "change_to_eplv2.sh" ! -path "./.git/*" \( -name debug.pr
     -e 's@http://www.eclipse.org/legal/epl-v10.html@https://www.eclipse.org/legal/epl-2.0/@' \
     {} +
 
-find . -type f ! -name "change_to_eplv2.sh" ! -name debug.product ! -path "./.git/*" ! -name "*.properties" \
+find . -type f ! -name "change_to_eplv2.sh" ! -path "./.git/*" \( -name "*.c" -or -name "*.h"  \) \
+  -exec sed -i -E \
+    -e 's@Eclipse Public License 1.0@Eclipse Public License 2.0@g' \
+    -e 's@Eclipse Public License v1.0@Eclipse Public License 2.0@g' \
+    -e 's@http://www.eclipse.org/legal/epl-v10.html@https://www.eclipse.org/legal/epl-2.0/@' \
+    {} +
+
+find . -type f ! -name "change_to_eplv2.sh" ! -name debug.product ! -path "./.git/*" ! -name "*.properties"  ! -name "template.xml" \
   -exec sed -i -E \
     -e 's@([[:blank:]]*)http://www.eclipse.org/legal/epl-v10.html@\1https://www.eclipse.org/legal/epl-2.0/\n\1\n\1SPDX-License-Identifier: EPL-2.0@' \
     {} +
@@ -44,3 +53,10 @@ for i in `find . -name epl-v10.html`; do
   rm $i
   cp /tmp/eplv2/license.html `dirname $i`/epl-v20.html
 done
+
+for i in `find . -name feature.properties`; do
+  sed -n '/URL of the "Feature License"/q;p' $i > /tmp/feature.properties
+  mv /tmp/feature.properties $i
+  cat /tmp/eplv2/feature.properties.txt >> $i
+done
+
