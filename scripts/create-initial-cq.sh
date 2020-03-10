702fff6d2331d37b29fbb1bdbfeb9e52e67834b2
Liviu Ionescu <ilg@livius.net>
2020-03-10 19:00:54 +0200

    add create-initial-cq.sh

---
+++ scripts/create-initial-cq.sh
@@ -0,0 +1,20 @@
+
+folder_name=embed-cdt-initial-cq-v2
+rm -rf ~/tmp/${folder_name}
+mkdir -p ~/tmp/${folder_name}
+cd ~/tmp/${folder_name}
+
+cp -r ~/tmp/eclipse-plugins.git/* .
+
+find . -name '*.jar' -exec rm -v {} \;
+echo
+for dir in $(find . -name templates -depth 3)
+do
+  echo
+  echo $dir
+  ls -l $dir
+  rm -rf $dir
+done
+
+cd ~/tmp
+tar czf ${folder_name}.tgz ${folder_name}
