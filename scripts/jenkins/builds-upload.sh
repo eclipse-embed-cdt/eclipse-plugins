ca7bc86c687badde65998b34ce62bed3142a81d3
Liviu Ionescu <ilg@livius.net>
2020-07-24 16:46:23 +0300

    jenkins/builds-upload.sh: add ls -l

--- scripts/jenkins/builds-upload.sh
+++ scripts/jenkins/builds-upload.sh
@@ -29,7 +29,8 @@ ${SCP} ${P2ZIP} ${SSHUSER}:${DOWNLOAD}-temp/ilg.gnumcueclipse.repository-${VERSI
 ${SSH} /bin/bash -x << _EOF_
 (
   cd ${DOWNLOAD}-temp
-
+  ls -lL
+  
   AP="$(ls ilg.gnumcueclipse.repository-*.zip)"
   shasum -a 256 -p "${AP}" >"${AP}.sha"
 
