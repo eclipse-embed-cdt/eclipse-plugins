b11f1e762045b716c3f543e2e046757348210853
Liviu Ionescu <ilg@livius.net>
2020-10-29 12:44:46 +0200

    [#419] - core: XpackUtils - add get*RepoBasePaths

--- plugins/ilg.gnumcueclipse.core/src/ilg/gnumcueclipse/core/XpackUtils.java
+++ plugins/ilg.gnumcueclipse.core/src/ilg/gnumcueclipse/core/XpackUtils.java
@@ -49,6 +49,7 @@ public class XpackUtils {
 		return true;
 	}
 
+	// DEPRECATED
 	public static IPath getRepoBasePath() {
 		
 		Map<String, String> env = System.getenv();
@@ -60,7 +61,7 @@ public class XpackUtils {
 		} else if (EclipseUtils.isLinux()) {
 			String homeFolder = env.get("HOME");
 			IPath path = new Path(homeFolder);
-			return path.append("/.local");
+			return path.append("/opt");
 		} else if (EclipseUtils.isWindows()) {
 			String homeFolder = env.get("APPDATA");
 			IPath path = new Path(homeFolder);
@@ -69,6 +70,31 @@ public class XpackUtils {
 		return null;
 	}
 
+	public static IPath[] getRepoBasePaths() {
+		
+		Map<String, String> env = System.getenv();
+		
+		if (EclipseUtils.isMacOSX()) {
+			IPath[] paths = new IPath[1];
+			String homeFolder = env.get("HOME");
+			paths[0] = (new Path(homeFolder)).append("/Library");
+			return paths;
+		} else if (EclipseUtils.isLinux()) {
+			IPath[] paths = new IPath[2];
+			String homeFolder = env.get("HOME");
+			paths[0] = (new Path(homeFolder)).append("/.local");
+			paths[1] = (new Path(homeFolder)).append("/opt");
+			return paths;
+		} else if (EclipseUtils.isWindows()) {
+			IPath[] paths = new IPath[1];
+			String homeFolder = env.get("APPDATA");
+			paths[0] = (new Path(homeFolder));
+			return paths;
+		}
+		return null;
+	}
+
+	// DEPRECATED
 	public static IPath getSysRepoBasePath() {
 		
 		Map<String, String> env = System.getenv();
@@ -87,22 +113,49 @@ public class XpackUtils {
 		return null;
 	}
 
+	public static IPath[] getSysRepoBasePaths() {
+		
+		Map<String, String> env = System.getenv();
+		
+		if (EclipseUtils.isMacOSX()) {
+			IPath[] paths = new IPath[1];
+			paths[0] = new Path("/opt");
+			return paths;
+		} else if (EclipseUtils.isLinux()) {
+			IPath[] paths = new IPath[1];
+			paths[0] = new Path("/opt");
+			return paths;
+		} else if (EclipseUtils.isWindows()) {
+			IPath[] paths = new IPath[1];
+			String progFolder = env.get("ProgramFiles");
+			paths[0] = new Path(progFolder);
+			return paths;
+		}
+		return null;
+	}
+
 	public static IPath getRepoPath() {
 
 		Map<String, String> env = System.getenv();
 		String folder = env.get("XPACKS_REPO_FOLDER");
 		
-		IPath path;
+		IPath repoPath;
 		if (folder != null) {
-			path = new Path(folder);
-			if (path.toFile().isDirectory()) {
-				return path;
+			repoPath = new Path(folder);
+			if (repoPath.toFile().isDirectory()) {
+				return repoPath;
 			}
 		}
 
-		path = getRepoBasePath();
-		if (path != null) {
-			return path.append("xPacks");
+		IPath[] paths;
+		paths = getRepoBasePaths();
+		if (paths != null) {
+			for (IPath path : paths) {
+				repoPath = path.append("xPacks");
+				if (repoPath.toFile().isDirectory()) {
+					return repoPath;
+				}
+			}
 		}
 
 		return null;
@@ -113,17 +166,23 @@ public class XpackUtils {
 		Map<String, String> env = System.getenv();
 		String folder = env.get("XPACKS_SYSTEM_FOLDER");
 		
-		IPath path;
+		IPath repoPath;
 		if (folder != null) {
-			path = new Path(folder);
-			if (path.toFile().isDirectory()) {
-				return path;
+			repoPath = new Path(folder);
+			if (repoPath.toFile().isDirectory()) {
+				return repoPath;
 			}
 		}
 
-		path = getSysRepoBasePath();
-		if (path != null) {
-			return path.append("xPacks");
+		IPath[] paths;
+		paths = getSysRepoBasePaths();
+		if (paths != null) {
+			for (IPath path : paths) {
+				repoPath = path.append("xPacks");
+				if (repoPath.toFile().isDirectory()) {
+					return repoPath;
+				}
+			}
 		}
 
 		return null;
