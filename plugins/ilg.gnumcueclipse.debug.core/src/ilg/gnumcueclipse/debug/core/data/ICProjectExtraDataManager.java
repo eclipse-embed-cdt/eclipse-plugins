d66243284cf7813e708a8a155d93ac449eff76e6
Liviu Ionescu <ilg@livius.net>
2020-11-03 16:25:57 +0200

    revert cprojectExtra to Activator.PLUGIN_ID

--- plugins/ilg.gnumcueclipse.debug.core/src/ilg/gnumcueclipse/debug/core/data/ICProjectExtraDataManager.java
+++ plugins/ilg.gnumcueclipse.debug.core/src/ilg/gnumcueclipse/debug/core/data/ICProjectExtraDataManager.java
@@ -26,9 +26,7 @@ public interface ICProjectExtraDataManager {
 	// ------------------------------------------------------------------------
 
 	public static final String EXTENSION_POINT_NAME = "cprojectExtra";
-	// public static final String EXTENSION_POINT_ID = ilg.gnumcueclipse.debug.core.Activator.PLUGIN_ID + "."
-	//		+ EXTENSION_POINT_NAME;
-	public static final String EXTENSION_POINT_ID = "org.eclipse.embedcdt.debug.core."
+	public static final String EXTENSION_POINT_ID = ilg.gnumcueclipse.debug.core.Activator.PLUGIN_ID + "."
 			+ EXTENSION_POINT_NAME;
 
 	// ------------------------------------------------------------------------
