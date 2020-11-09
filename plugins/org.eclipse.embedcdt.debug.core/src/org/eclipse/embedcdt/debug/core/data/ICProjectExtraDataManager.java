724f6ec5e8d131536943610d9f82698d5ca6bfc9
Liviu Ionescu <ilg@livius.net>
2020-11-09 20:51:23 +0200

    rename cprojectExtra extension point

--- plugins/org.eclipse.embedcdt.debug.core/src/org/eclipse/embedcdt/debug/core/data/ICProjectExtraDataManager.java
+++ plugins/org.eclipse.embedcdt.debug.core/src/org/eclipse/embedcdt/debug/core/data/ICProjectExtraDataManager.java
@@ -26,8 +26,7 @@ public interface ICProjectExtraDataManager {
 	// ------------------------------------------------------------------------
 
 	public static final String EXTENSION_POINT_NAME = "cprojectExtra";
-	public static final String EXTENSION_POINT_ID = "ilg.gnumcueclipse.debug.core."
-			+ EXTENSION_POINT_NAME;
+	public static final String EXTENSION_POINT_ID = "org.eclipse.embedcdt.debug.core.cprojectExtra";
 
 	// ------------------------------------------------------------------------
 
