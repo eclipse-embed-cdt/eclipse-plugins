af335426e3e0b7fef51d6550aab3f02c9fad1f64
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2020-11-24 17:25:18 +0300

    #428 Split Java code between core & UI plug-ins
    Extract "org.eclipse.embedcdt.debug.gdbjtag.openocd.ui" bundle.
    
    Signed-off-by: Alexander Fedorov <alexander.fedorov@arsysop.ru>

--- plugins/org.eclipse.embedcdt.debug.gdbjtag.openocd/src/org/eclipse/embedcdt/debug/gdbjtag/openocd/Activator.java
+++ plugins/org.eclipse.embedcdt.debug.gdbjtag.openocd/src/org/eclipse/embedcdt/debug/gdbjtag/openocd/Activator.java
@@ -14,15 +14,15 @@
 
 package org.eclipse.embedcdt.debug.gdbjtag.openocd;
 
+import org.eclipse.embedcdt.core.AbstractActivator;
 import org.eclipse.embedcdt.debug.gdbjtag.openocd.preferences.DefaultPreferences;
 import org.eclipse.embedcdt.debug.gdbjtag.openocd.preferences.PersistentPreferences;
-import org.eclipse.embedcdt.ui.AbstractUIActivator;
 import org.osgi.framework.BundleContext;
 
 /**
  * The activator class controls the plug-in life cycle
  */
-public class Activator extends AbstractUIActivator {
+public class Activator extends AbstractActivator {
 
 	// ------------------------------------------------------------------------
 
